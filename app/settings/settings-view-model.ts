c62148949ebdbe13e3107a8c192efff3966b3486
James Hooper <jameshooperdesigns@gmail.com>
2019-05-23 17:33:44 -0600

    Add list to settings view model

--- app/settings/settings-view-model.ts
+++ app/settings/settings-view-model.ts
@@ -1,7 +1,24 @@
+const observableModule = require("tns-core-modules/data/observable");
+const ObservableArray = require("tns-core-modules/data/observable-array").ObservableArray;
+
 import { Observable } from "tns-core-modules/data/observable";
 
 export class SettingsViewModel extends Observable {
     constructor() {
         super();
+        
+        const viewModel = observableModule.fromObject({
+                    data: new ObservableArray([
+                        { text: "Sign-In / Create Account"},
+                        { text: "Map"},
+                        { text: "Delays"},
+                        { text: "Privacy"},
+                        { text: "Notifications"},
+                        { text: "Feedback"}
+                    ])
+                });
+                return viewModel;
+
     }
 }
+
